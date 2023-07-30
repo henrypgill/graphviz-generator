23bdd71f7181b8e0e5c33504b34a2c45e70ce422
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-07-30 15:00:02 +0100

    formatted testing files

--- src/testing/GraphVizStrings.spec.ts
+++ src/testing/GraphVizStrings.spec.ts
@@ -2,6 +2,6 @@
 import { createNode } from "../components/graphElements";
 
 test("test a string is returned", () => {
-    const node = createNode("string")
+    const node = createNode("string");
     expect(node.name).toEqual("string");
 });
